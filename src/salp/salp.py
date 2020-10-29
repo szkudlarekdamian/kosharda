7ef29bc438d468f3e5b6dabcc6abb59ae30dce92
Damian Szkudlarek <38192105+szkudlarekdamian@users.noreply.github.com>
2020-10-29 02:24:37 +0100

    Revert "salp alpha 6.9"
    This reverts commit 27bbb2f07acbbcb7fce2d99800895520e1c421ce.

--- src/salp/salp.py
+++ src/salp/salp.py
@@ -1,92 +1,14 @@
-import numpy as np
-from typing import Tuple
-from generation.gamma import Generator
+from src.generation.multivariate_normal import random_corelated_vectors
 
+# TEST
+# skala dla 3 rozkładów
+mv = [0,1,2] # średnie
+vv = [1,4,9] # wariancje
+co = 0.8 # korelacja
+si = 100 # ilość próbek
 
-class Endpoint:
-    def __init__(self, identity: int):
-        self.identity = identity
-        self.active = True
-        # shards
-        self.fs = np.array([])
-        # load vector
-        self.ws = np.array([0.])
+tmp = random_corelated_vectors(mv,vv,cor=co,size=si)
 
-    def get_modules_difference(self, w, nwts):
-        ws = self.ws.copy()
-        x1 = np.linalg.norm(ws - nwts)
-        x2 = np.linalg.norm(ws + w - nwts)
-        return x1 - x2
-
-    def shard_append(self, f, w, nwts):
-        self.fs = np.append(self.fs, f)
-        self.ws.resize(w.shape)
-        self.ws += w
-        if np.linalg.norm(self.ws) > np.linalg.norm(nwts):
-            self.active = False
-
-
-class SALP:
-    def __init__(self, n: int, f: int, s: int, cor: float, scals_range: Tuple[float, float]) -> None:
-        generator = Generator(f, s, cor, scals_range)
-        self.shards_orig = generator.generate_cloud_load_vectors()
-        self.wts = self.shards_orig.sum(axis=0)
-        self.nwts = self.wts / n
-        self.lw = self.sort_shards_by_module(self.shards_orig)
-        self.endpoints = [Endpoint(e) for e in range(0, n)]
-
-        self.print_basics()
-
-        self.arrange_shards()
-
-    def print_basics(self):
-        print("\tShards: {}\n\n\t"
-              "WTS: {}\n\n\t"
-              "NWTS: {}\n\n\t"
-              "LW: {}\n\n\t"
-              .format(self.shards_orig, self.wts, self.nwts, self.lw))
-
-
-    def sort_shards_by_module(self, shards):
-        modules = [(i, np.linalg.norm(m)) for i, m in enumerate(shards)]
-        return sorted(modules, key=lambda x: x[1], reverse=True)
-
-    def arrange_shards(self):
-        for shard_id, _ in self.lw:
-            shard = self.shards_orig[shard_id]
-            print(shard_id)
-            max_delta = float("-inf")
-            max_id = -1
-            for endpoint in self.endpoints:
-                if endpoint.active:
-                    delta = endpoint.get_modules_difference(shard, self.nwts)
-                    if delta > max_delta:
-                        max_id = endpoint.identity
-                    print("Shard: {}\n"
-                          "Endpoint: {}\n"
-                          "Delta: {}\n\n"
-                          .format(shard,
-                                  endpoint.identity,
-                                  delta))
-                else:
-                    print("ENDPOINT {} NOT ACTIVE".format(endpoint.identity))
-            if max_id >= 0:
-                self.endpoints[max_id].shard_append(shard_id, shard, self.nwts)
-            else:
-                print("MAX_ID < 0 ({})".format(max_id))
-        for endpoint in self.endpoints:
-            print("Endpoint: {}\n"
-                  "Shards: {}\n"
-                  "Load: {}\n\n"
-                  .format(endpoint.identity,
-                          endpoint.fs,
-                          endpoint.ws))
-
-
-if __name__ == '__main__':
-    endpoints = 4
-    shards = 10
-    shards_history = 12
-    correlation = 1.0
-
-    SALP(endpoints, shards, shards_history, correlation, (2.0, 4.0))
+assert len(tmp) == len(mv) == len(vv) # 3 rozkłady
+assert len(tmp[0]) == si # długość pierwszego (każdego) równa się size
+assert tmp.size == len(mv) * si
