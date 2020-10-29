fa8df67b12e488cbbb2db2acb1b4340a6c34d31b
Damian Szkudlarek <38192105+szkudlarekdamian@users.noreply.github.com>
2020-10-29 11:48:47 +0100

    Docs added to SALP

--- src/salp/salp.py
+++ src/salp/salp.py
@@ -1,24 +1,45 @@
 import numpy as np
-from typing import Tuple
 from generation.gamma import Generator
 
 
-class Endpoint:
-    def __init__(self, identity: int):
-        self.identity = identity
-        self.active = True
-        # shards
-        self.fs = np.array([])
-        # load vector
-        self.ws = np.array([0.])
+class Node:
+    """
+    Node, which is a part of cloud.
 
-    def get_modules_difference(self, w, nwts):
+   :param int identity: ID of a node
+   :param bool active: Determines whether any more shards can be assigned to the node
+   :param numpy.ndarray fs: List of ids of shards assigned to the node
+   :param numpy.ndarray ws: Summed load vector of shards assigned to the node
+    """
+
+    def __init__(self, identity: int, active: bool = True, fs: np.ndarray = np.array([]),
+                 ws: np.ndarray = np.array([0.])) -> None:
+        self.identity = identity
+        self.active = active
+        self.fs = fs
+        self.ws = ws
+
+    def get_modules_difference(self, w: np.ndarray, nwts: np.ndarray) -> np.ndarray:
+        """
+        :param numpy.ndarray w: Load vector of a candidate shard
+        :param numpy.ndarray nwts: Normalized load vector of a cloud
+        :return numpy.ndarray: Difference between modules of [difference between node load vector and normalized cloud load vector]
+                                and [also difference between the former, but node load vector includes candidate shard]
+        """
         ws = self.ws.copy()
         x1 = np.linalg.norm(ws - nwts)
         x2 = np.linalg.norm(ws + w - nwts)
         return x1 - x2
 
-    def shard_append(self, f, w, nwts):
+    def shard_append(self, f: int, w: np.ndarray, nwts: np.ndarray) -> None:
+        """
+        Appends shard to the node. After this, the module of node load vector is calculated -
+            when its value is greater than the module of normalized cloud load vector, the node becomes inactive.
+
+        :param int f: ID of an appended shard
+        :param numpy.ndarray w: Load vector of an appended shard
+        :param numpy.ndarray nwts: Normalized load vector of a cloud
+        """
         self.fs = np.append(self.fs, f)
         self.ws.resize(w.shape)
         self.ws += w
@@ -27,66 +48,89 @@ class Endpoint:
 
 
 class SALP:
-    def __init__(self, n: int, f: int, s: int, cor: float, scals_range: Tuple[float, float]) -> None:
-        generator = Generator(f, s, cor, scals_range)
-        self.shards_orig = generator.generate_cloud_load_vectors()
+    """
+    Implementation of SALP (Shards Allocation based on Load Prediction)
+
+    :param int n: Number of nodes, to which shards will be assigned.
+    :param numpy.ndarray shards: Load vectors of shards
+
+    wts: Summed vector of loads of the cloud
+    nwts: Normalized wts (Mean load vector of a single node)
+    lw: List of tuples[shard_id, shard_module], sorted in descending order by the value of shard module
+    nodes: List of nodes of a cloud
+    """
+    def __init__(self, n: int, shards: np.ndarray) -> None:
+        self.shards_orig = shards
         self.wts = self.shards_orig.sum(axis=0)
         self.nwts = self.wts / n
-        self.lw = self.sort_shards_by_module(self.shards_orig)
-        self.endpoints = [Endpoint(e) for e in range(0, n)]
+        self.lw = self.__sort_shards_by_module(self.shards_orig)
+        self.nodes = [Node(e) for e in range(0, n)]
 
         self.print_basics()
 
-        self.arrange_shards()
+        self.__arrange_shards()
 
-    def print_basics(self):
+    def print_basics(self) -> None:
+        """Print shards, wts, nwts and lw"""
         print("\tShards: {}\n\n\t"
               "WTS: {}\n\n\t"
               "NWTS: {}\n\n\t"
               "LW: {}\n\n\t"
               .format(self.shards_orig, self.wts, self.nwts, self.lw))
 
-
-    def sort_shards_by_module(self, shards):
+    def __sort_shards_by_module(self, shards) -> list:
+        """
+        :param shards: Shards to be sorted
+        :return: List of tuples[shard_id, shard_module], sorted in descending order by the value of shard module
+        """
         modules = [(i, np.linalg.norm(m)) for i, m in enumerate(shards)]
         return sorted(modules, key=lambda x: x[1], reverse=True)
 
-    def arrange_shards(self):
+    def __arrange_shards(self) -> None:
+        """
+        Assigns shards to nodes.
+        Each shard is assigned to the node with the highest delta (obtained from Node.get_modules_difference).
+        This function updates nodes list (self.nodes)
+        :return:
+        """
         for shard_id, _ in self.lw:
             shard = self.shards_orig[shard_id]
             print(shard_id)
             max_delta = float("-inf")
             max_id = -1
-            for endpoint in self.endpoints:
-                if endpoint.active:
-                    delta = endpoint.get_modules_difference(shard, self.nwts)
+            for node in self.nodes:
+                if node.active:
+                    delta = node.get_modules_difference(shard, self.nwts)
                     if delta > max_delta:
-                        max_id = endpoint.identity
+                        max_id = node.identity
                     print("Shard: {}\n"
-                          "Endpoint: {}\n"
+                          "Node: {}\n"
                           "Delta: {}\n\n"
                           .format(shard,
-                                  endpoint.identity,
+                                  node.identity,
                                   delta))
                 else:
-                    print("ENDPOINT {} NOT ACTIVE".format(endpoint.identity))
+                    print("NODE {} NOT ACTIVE".format(node.identity))
             if max_id >= 0:
-                self.endpoints[max_id].shard_append(shard_id, shard, self.nwts)
+                self.nodes[max_id].shard_append(shard_id, shard, self.nwts)
             else:
                 print("MAX_ID < 0 ({})".format(max_id))
-        for endpoint in self.endpoints:
-            print("Endpoint: {}\n"
+        for node in self.nodes:
+            print("Node: {}\n"
                   "Shards: {}\n"
                   "Load: {}\n\n"
-                  .format(endpoint.identity,
-                          endpoint.fs,
-                          endpoint.ws))
+                  .format(node.identity,
+                          node.fs,
+                          node.ws))
 
 
 if __name__ == '__main__':
-    endpoints = 4
-    shards = 10
-    shards_history = 12
-    correlation = 1.0
+    _nodes = 4
+    _shards_num = 10
+    _load_history_length = 12
+    _correlation = 1.0
 
-    SALP(endpoints, shards, shards_history, correlation, (2.0, 4.0))
+    generator = Generator(_shards_num, _load_history_length, _correlation, (2.0, 4.0))
+    _shards = generator.generate_cloud_load_vectors()
+    
+    SALP(_nodes, _shards)
